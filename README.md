e0a5d5963f4c25d8eeac53845cab9202c660d2fb
venite-xjc <861624175@qq.com>
2022-08-06 00:07:58 +0800

    1.0.0

---
+++ README.md
@@ -0,0 +1,21 @@
+# visualpano README
+
+This extension aims to provide a panorama view for a single image.
+该插件能够将单张图转化为$360\degree$全景视图。
+
+
+右键图片，使用
+`VisualPano: Start a panorama view | 开启全景`
+命令，打开webveiw
+
+---
+### 1.0.0
+
+仅支持简单的预览，不支持自动焦点更新
+
+
+---
+based on:
+- three.js
+- panolens.js
+
---
+++ src/extension.js
@@ -0,0 +1,81 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const path = require('path');
+const fs = require('fs')
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	let currentPanel = undefined;
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('VisualPano.start', (uri)=>{
+			
+			if (currentPanel) {
+				currentPanel.dispose();
+				currentPanel = undefined;
+			} 
+				currentPanel = vscode.window.createWebviewPanel(
+					'panorama',
+					'panorama',
+					vscode.ViewColumn.Two,
+					{
+						enableScripts: true, 
+            			retainContextWhenHidden: true,
+					}
+				);
+				currentPanel.webview.html = getWebViewContent(context, './src/index.html');
+
+				currentPanel.onDidDispose(
+					() => {
+						currentPanel = undefined;
+					},
+					null,
+					context.subscriptions
+				);
+			
+			if(uri) {
+			currentPanel.webview.postMessage({text: currentPanel.webview.asWebviewUri(uri).toString(), message: 'no'});
+			}
+		})
+	);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {
+
+}
+
+
+function getWebviewFileResource(context, panel, relativePath) {
+	const onDiskPath = vscode.Uri.file(
+        path.join(context.extensionPath, relativePath)
+      );
+	let vscodeUriPath = panel.webview.asWebviewUri(onDiskPath);
+	let webviewResourceUri = onDiskPath.with({ scheme: 'vscode-resource' }).toString();
+	return webviewResourceUri;
+}
+
+function getWebViewContent(context, templatePath) {
+
+    const resourcePath = path.join(context.extensionPath, templatePath);
+    const dirPath = path.dirname(resourcePath);
+    let html = fs.readFileSync(resourcePath, 'utf-8');
+	console.log(html);
+    html = html.replace(/(<link.+?href="|<script.+?src="|<iframe.+?src="|<img.+?src=")(.+?)"/g, (m, $1, $2) => {
+        if($2.indexOf("https://")<0)return $1 + vscode.Uri.file(path.resolve(dirPath, $2)).with({ scheme: 'vscode-resource' }).toString() + '"';
+        else return $1 + $2+'"';
+    });
+	console.log(html);
+    return html;
+}
+
+module.exports = {
+	activate,
+	deactivate
+}
